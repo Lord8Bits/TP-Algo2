5f53e0c3590b709b79fbbfcb00afa027a5f82db5
Lord8Bits <62944891+Lord8Bits@users.noreply.github.com>
2025-04-28 01:58:38 +0100

    Added factorial function

--- TP2.py
+++ TP2.py
@@ -223,4 +223,9 @@ def anagramme(str1, str2) -> bool:
     else:
         return True
 
-print(anagramme("chien", "nich"))
+# EX 5:
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return n*factorial(n-1)
+print(factorial(3))
